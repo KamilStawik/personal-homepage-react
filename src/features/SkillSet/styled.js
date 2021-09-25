6bfb2cbe700e5a1bbe5ca1b651749671d2ef679c
Kamil Stawik <kamil_stawik@op.pl>
2021-09-25 13:40:30 +0200

    Styled components added to SkillSet

---
+++ src/features/SkillSet/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  max-width: 1216px;
+  padding: 32px;
+  margin: 66px auto;
+`;
+
+export const SectionHeader = styled.h2`
+  font-weight: 900;
+  font-size: 30px;
+  margin: 0px 0px 48px 0px;
+  letter-spacing: 0.05em;
+  color: ${({ theme }) => (theme.colors.mineShaft)};
+`;
+
+export const UnorderedList = styled.ul`
+  padding-left: 20px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const ListItem = styled.li`
+  flex-basis: 33%;
+`;
