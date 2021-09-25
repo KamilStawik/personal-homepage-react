133f949eea1f0ef03b319616987a5676746d943e
Kamil Stawik <kamil_stawik@op.pl>
2021-09-25 13:39:54 +0200

    SkillSet content added

---
+++ src/features/SkillSet/index.js
@@ -0,0 +1,34 @@
+import { ListItem, SectionHeader, UnorderedList, Wrapper } from "./styled"
+
+
+const SkillSet = () => (
+  <Wrapper>
+    <SectionHeader>My skillset includes 🛠️</SectionHeader>
+    <UnorderedList>
+      <ListItem>Semantic & accessible HTML</ListItem>
+      <ListItem>React Router</ListItem>
+      <ListItem>Promises, Async/Await</ListItem>
+      <ListItem>Responsive Web Design</ListItem>
+      <ListItem>Redux-Saga</ListItem>
+      <ListItem>GitHub Pull Requests & Review</ListItem>
+      <ListItem>Teamwork</ListItem>
+      <ListItem>Redux (Toolkit)</ListItem>
+      <ListItem>Trello</ListItem>
+      <ListItem>Markdown</ListItem>
+      <ListItem>React Hooks</ListItem>
+      <ListItem>Scrum</ListItem>
+      <ListItem>Immutability</ListItem>
+      <ListItem>Error handling</ListItem>
+      <ListItem>NPM</ListItem>
+      <ListItem>CSS BEM convention</ListItem>
+      <ListItem>Working with API (fetch, axios)</ListItem>
+      <ListItem>React</ListItem>
+      <ListItem>CSS Grid</ListItem>
+      <ListItem>JavaScript: ES6+</ListItem>
+      <ListItem>Git</ListItem>
+      <ListItem>CSS Flexbox</ListItem>
+    </UnorderedList>
+  </Wrapper>
+);
+
+export default SkillSet;
