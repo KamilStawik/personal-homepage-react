ad67cb799cec74b0862faa3728cdc54b7a7e4aba
Kamil Stawik <kamil_stawik@op.pl>
2021-09-27 15:25:25 +0200

    skills list moved to new file skillSetContent

--- src/features/SkillSet/index.js
+++ src/features/SkillSet/index.js
@@ -1,32 +1,14 @@
-import { ListItem, ListItemText, SectionHeader, UnorderedList, Wrapper } from "./styled"
+import { ListItem, ListItemText, SectionHeader, UnorderedList, Wrapper } from "./styled";
+import skillSetContent from "./skillSetContent";
 
 
 const SkillSet = () => (
   <Wrapper>
     <SectionHeader>My skillset includes 🛠️</SectionHeader>
     <UnorderedList>
-      <ListItem><ListItemText>Semantic & accessible HTML</ListItemText></ListItem>
-      <ListItem><ListItemText>React Router</ListItemText></ListItem>
-      <ListItem><ListItemText>Promises, Async/Await</ListItemText></ListItem>
-      <ListItem><ListItemText>Responsive Web Design</ListItemText></ListItem>
-      <ListItem><ListItemText>Redux-Saga</ListItemText></ListItem>
-      <ListItem><ListItemText>GitHub Pull Requests & Review</ListItemText></ListItem>
-      <ListItem><ListItemText>Teamwork</ListItemText></ListItem>
-      <ListItem><ListItemText>Redux (Toolkit)</ListItemText></ListItem>
-      <ListItem><ListItemText>Trello</ListItemText></ListItem>
-      <ListItem><ListItemText>Markdown</ListItemText></ListItem>
-      <ListItem><ListItemText>React Hooks</ListItemText></ListItem>
-      <ListItem><ListItemText>Scrum</ListItemText></ListItem>
-      <ListItem><ListItemText>Immutability</ListItemText></ListItem>
-      <ListItem><ListItemText>Error handling</ListItemText></ListItem>
-      <ListItem><ListItemText>NPM</ListItemText></ListItem>
-      <ListItem><ListItemText>CSS BEM convention</ListItemText></ListItem>
-      <ListItem><ListItemText>Working with API (fetch, axios)</ListItemText></ListItem>
-      <ListItem><ListItemText>React</ListItemText></ListItem>
-      <ListItem><ListItemText>CSS Grid</ListItemText></ListItem>
-      <ListItem><ListItemText>JavaScript: ES6+</ListItemText></ListItem>
-      <ListItem><ListItemText>Git</ListItemText></ListItem>
-      <ListItem><ListItemText>CSS Flexbox</ListItemText></ListItem>
+      {skillSetContent.map(skill => (
+        <ListItem key={skill.id}><ListItemText>{skill.content}</ListItemText></ListItem>
+      ))}
     </UnorderedList>
   </Wrapper>
 );
---
+++ src/features/SkillSet/skillSetContent.js
@@ -0,0 +1,27 @@
+const skillSetContent =
+    [
+        { id: 1, content: "Semantic & accessible HTML" },
+        { id: 2, content: "React Router" },
+        { id: 3, content: "Promises, Async/Await" },
+        { id: 4, content: "Responsive Web Design" },
+        { id: 5, content: "Redux-Saga" },
+        { id: 6, content: "GitHub Pull Requests & Review" },
+        { id: 7, content: "Teamwork" },
+        { id: 8, content: "Redux (Toolkit)" },
+        { id: 9, content: "Trello" },
+        { id: 10, content: "Markdown" },
+        { id: 11, content: "React Hooks" },
+        { id: 12, content: "Scrum" },
+        { id: 13, content: "Immutability" },
+        { id: 14, content: "Error handling" },
+        { id: 15, content: "NPM" },
+        { id: 16, content: "CSS BEM convention" },
+        { id: 17, content: "Working with API (fetch, axios)" },
+        { id: 18, content: "React" },
+        { id: 19, content: "CSS Grid" },
+        { id: 20, content: "JavaScript: ES6+" },
+        { id: 21, content: "Git" },
+        { id: 22, content: "CSS Flexbox" },
+    ]
+
+export default skillSetContent;
