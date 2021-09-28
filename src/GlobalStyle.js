7e22de5f38f771bb45aad8ccd273ec9111a6e0b1
Kamil Stawik <kamil_stawik@op.pl>
2021-09-28 13:47:09 +0200

    GlobalStyle.js added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    html {
+        box-sizing: border-box;
+    }
+    
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        margin: 0;
+        font-family: "Inter", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './GlobalStyle';
 import { theme } from "./theme"
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
