a3c5ec532dbf97add6f3f6ebd95e11b1843c2c08
Kamil Stawik <kamil_stawik@op.pl>
2021-09-29 15:21:40 +0200

    fetching data from API moved to Portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // import logo from './logo.svg';
-import { useEffect, useState } from 'react';
 import Container from './common/Container';
 import PersonalInfo from './features/PersonalInfo';
 import SkillsSection from './features/SkillsSection';
@@ -8,45 +7,12 @@ import futureSkillsContent from './futureSkillsContent'
 import Portfolio from './features/Portfolio';
 
 function App() {
-
-  const [response, setResponse] = useState("");
-  const [appStatus, setAppStatus] = useState("loading");
-  const demoDelay = 5000;
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("https://api.github.com/users/KamilStawik/repos")
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(response.statusText);
-          }
-          return response;
-        })
-        .then(response => response.json())
-        .then(response => setResponse(response))
-        .then(setAppStatus("succes"))
-        .catch(error =>
-          console.log(error));
-    }, demoDelay);
-  }, []);
-
-  setTimeout(() => {
-    console.log(response);
-  }, 10000);
-
   return (
     <Container>
       <PersonalInfo />
       <SkillsSection title={"My skillset includes 🛠️"} content={skillSetContent} />
       <SkillsSection title={"What I want to learn next"} content={futureSkillsContent} />
       <Portfolio />
-      <ul>
-        {appStatus === "succes" &&
-          response.map(repository => (
-            <li key={repository.id}>{repository.name}, {repository.html_url}</li>
-          ))
-        }
-      </ul>
     </Container>
   );
 }
--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -1,13 +1,60 @@
+import { useEffect, useState } from 'react';
 import { GitHubIcon } from "./GitHubIcon/index.js";
 import SectionHeader from "../../common/SectionHeader/index.js";
 import { Subtitle } from "./styled.js";
 
-const Portfolio = () => (
-    <>
-        <GitHubIcon />
-        <SectionHeader title={"Portfolio"} portfolio />
-        <Subtitle>My recent projects</Subtitle>
-    </>
-);
+const Portfolio = () => {
+
+    const initialData = [
+        {
+            id: 1,
+            name: "test1",
+            html_url: "test1",
+        },
+        {
+            id: 2,
+            name: "test2",
+            html_url: "test2",
+        },
+    ];
+
+    const [response, setResponse] = useState(initialData);
+    const [appStatus, setAppStatus] = useState("loading");
+    const demoDelay = 2000;
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("https://api.github.com/users/KamilStawik/repos")
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response;
+                })
+                .then(response => response.json())
+                .then(response => {
+                    setResponse(response)
+                    console.log(response)
+                })
+                .then(setAppStatus("succes"))
+                .catch(error =>
+                    console.log(error));
+        }, demoDelay);
+    }, []);
+
+    return (
+        <>
+            <GitHubIcon />
+            <SectionHeader title={"Portfolio"} portfolio />
+            <Subtitle>My recent projects</Subtitle>
+
+            {appStatus === "succes" &&
+                response.map(repository => (
+                    <li key={repository.id}>{repository.name},  {repository.html_url}</li>
+                ))
+            }
+        </>
+    );
+}
 
 export default Portfolio;
