5fafe757b8fa1e102c2418fae68bed79986c4086
Kamil Stawik <kamil_stawik@op.pl>
2021-10-09 12:13:57 +0200

    DarkModeToggler reworked

--- src/features/homepage/PersonalInfo/DarkModeToggler/index.js
+++ src/features/homepage/PersonalInfo/DarkModeToggler/index.js
@@ -1,13 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDarkTheme, toggleDarkTheme } from '../../homepageSlice';
 import { ReactComponent as DarkModeOff } from '../../../../images/darkModeOff.svg';
 import { ReactComponent as DarkModeOn } from '../../../../images/darkModeOn.svg';
 import TopText from './../../../../common/TopText'
 import { StyledButton } from './styled';
 
-const DarkModeToggler = () => (
-    <StyledButton>
-        <TopText>dark mode off</TopText>
-        <DarkModeOff />
-    </StyledButton>
-);
+const DarkModeToggler = () => {
 
+    const dispatch = useDispatch();
+    const darkTheme = useSelector(selectDarkTheme);
+
+    return (
+        <StyledButton onClick={() => dispatch(toggleDarkTheme())} >
+            <TopText>{darkTheme ? "dark mode on" : "dark mode off"}</TopText>
+            {darkTheme ? <DarkModeOn /> : <DarkModeOff />}
+        </StyledButton >
+    );
+}
 export default DarkModeToggler;
