e74aac0da290d02d9400bebf1a7211831c6c1d31
Kamil Stawik <kamil_stawik@op.pl>
2021-11-28 20:21:21 +0100

    code cleaning

--- src/Homepage/LastCommitTimer/useGetTime.js
+++ src/Homepage/LastCommitTimer/useGetTime.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { selectApplicationStatus, selectRepositories } from "./../homepageSlice";
 
 const useGetTime = () => {
-
     const repositories = useSelector(selectRepositories);
     const applicationStatus = useSelector(selectApplicationStatus);
 
@@ -12,37 +11,28 @@ const useGetTime = () => {
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
 
-    let sortedRepositories = [];
-
     const timePeriods = [
         {
             name: "days",
             seconds: 86400,
-            value: 0,
         },
         {
             name: "hours",
             seconds: 3600,
-            value: 0,
         },
         {
             name: "minutes",
             seconds: 60,
-            value: 0,
         },
         {
             name: "seconds",
             seconds: 1,
-            value: 0,
         },
     ];
 
-    const sortRepositories = () => {
-        repositories.forEach(repository => sortedRepositories.push(new Date(repository.updated_at)));
-        sortedRepositories = sortedRepositories.sort((a, b) => b - a);
-    };
-
-    sortRepositories();
+    let sortedRepositories = [];
+    repositories.forEach(repository => sortedRepositories.push(new Date(repository.updated_at)));
+    sortedRepositories = sortedRepositories.sort((a, b) => b - a);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -53,22 +43,18 @@ const useGetTime = () => {
                         case "days":
                             setDays((Math.floor(timeDifference / timePeriod.seconds)).toFixed());
                             timeDifference = timeDifference % timePeriod.seconds;
-                            console.log(`ustawiono dni na ${days}`);
                             break;
                         case "hours":
                             setHours((Math.floor(timeDifference / timePeriod.seconds)).toFixed());
                             timeDifference = timeDifference % timePeriod.seconds;
-                            console.log(`ustawiono godziny na ${hours}`);
                             break;
                         case "minutes":
                             setMinutes((Math.floor(timeDifference / timePeriod.seconds)).toFixed());
                             timeDifference = timeDifference % timePeriod.seconds;
-                            console.log(`ustawiono minuty na ${minutes}`);
                             break;
                         default:
                             setSeconds((Math.floor(timeDifference / timePeriod.seconds)).toFixed());
                             timeDifference = timeDifference % timePeriod.seconds;
-                            console.log(`ustawiono sekundy na ${seconds}`);
                             break;
                     };
                 });
@@ -77,15 +63,6 @@ const useGetTime = () => {
             clearInterval(intervalId);
         };
     }, [applicationStatus]);
-
-
-    // const days = timePeriods.find(timePeriod => timePeriod.name === "day").value;
-    // const hours = timePeriods.find(timePeriod => timePeriod.name === "hour").value;
-    // const minutes = timePeriods.find(timePeriod => timePeriod.name === "minute").value;
-    // const seconds = timePeriods.find(timePeriod => timePeriod.name === "second").value;
-
-    console.log(`wynik z getTime ${seconds}`);
-
 
     return { days, hours, minutes, seconds };
 };
