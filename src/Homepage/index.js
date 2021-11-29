ab9105d97bf4c3be03d0346638336f769105e8d9
Kamil Stawik <kamil_stawik@op.pl>
2021-11-29 10:07:27 +0100

    FutureSkillsSection Removed

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -2,7 +2,6 @@ import Container from './../common/Container';
 import PersonalInfo from './PersonalInfo';
 import SkillsSection from './SkillsSection';
 import skillSetContent from './content/skillSetContent';
-import futureSkillsContent from './content/futureSkillsContent';
 import LastCommitTimer from './LastCommitTimer';
 import Portfolio from './Portfolio';
 import Footer from './Footer';
@@ -12,7 +11,6 @@ const Homepage = () => (
     <Container>
         <PersonalInfo />
         <SkillsSection title={"My skillset includes"} content={skillSetContent} />
-        <SkillsSection title={"What I want to learn next"} content={futureSkillsContent} />
         <FutureSkills />
         <LastCommitTimer />
         <Portfolio />
